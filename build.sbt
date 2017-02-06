fdf2b38045ab1633de7df64ad031f761b84f60fd
starqiu <674867812@qq.com>
2017-02-06 10:05:19 +0800

    add hbase test

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "LearningScala"
 
 version := "1.0"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.11.8"
 
 // https://mvnrepository.com/artifact/com.typesafe.akka/akka-actor_2.11
 libraryDependencies += "com.typesafe.akka" % "akka-actor_2.11" % "2.4.9"
--- test/src/test.scala
+++ test/src/test.scala
@@ -1,10 +1,9 @@
 /**
   * Created by starqiu on 16-11-13.
   */
-class hbaseTest {
+class test {
 
 }
-
 object hbaseTest extends App{
   val line1 = "k1\ta1\tb1"
   val line2 = "k1\ta2\tb1"
@@ -13,12 +12,14 @@ object hbaseTest extends App{
 
   val lineg = lines.groupBy(line=>line.split("\t")(0))
   val schemasWithId = Array("c1\t1","c2\t1","c3\t1")
-  val l = lineg.map(klines => {
-    (klines._1, klines._2.flatMap(line => schemasWithId zip line.split("\t") filter(_._1.equals("c1"))).distinct)
+  val l = lineg.mapValues(lines => {
+    lines.flatMap(line => schemasWithId zip line.split("\t") filterNot(_._1.equals("c1\t1"))).distinct
   })
 
+  val g = l.mapValues(_.groupBy(_._1).mapValues(vs =>vs.map(_._2).mkString("\t")))
+
   l.foreach(sv=> {
-    print(s"${sv._1},")
-    sv._2.foreach(v => print(v._1 + "," + v._2 + "\n"))
+    print(s"${sv._1}\n")
+    sv._2.foreach(v => print("\t"+v._1 + "," + v._2 + "\n"))
   })
 }
